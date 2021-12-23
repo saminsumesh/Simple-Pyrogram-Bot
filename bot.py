58202e7edfe1528f04d393d83b2fc83cd9c18cc5
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-12-23 18:28:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,22 @@
 import os 
 from pyrogram import Client, filters
 
+BOT_TOKEN = # your bot token from telegram.me/BotFather. Sample :- "12345:abcdefghijklmnop"
+API_ID = # your api id from my.telegram.org. Sample :- int("123456")
+API_HASH = # your api hash from my.telegram.org Sample :- "fayasnoushad123"
+
+
 Bot = Client(
-    "Simple pyrogram bot"
-    bot_token = os.environ.get("BOT_TOKEN")
-    api_hash = int(os.enivron.get("API_HASH"),
-    api_key = os.environ.get("API_KEY")
+    "Simple-Pyrogram-Bot",
+    bot_token=BOT_TOKEN,
+    api_id=int(API_ID),
+    api_hash=API_HASH
 )
 
-@Bot.on_message(filters.private & filters.command(["start"]))
+@Bot.on_message(filters.command(["start"]))
 async def start(bot, update):
     await update.reply_text(
-    text=f"HI {update.from_user.mention}
-  )
+        text=f"Hi {update.from_user.mention}"
+    )
     
-    Bot.run()
+ Bot.run()
