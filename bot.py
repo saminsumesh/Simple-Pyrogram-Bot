b042ad7155a4a202ce0fdb1a6344f67cf75c0da2
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-12-23 18:56:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,13 +2,13 @@ import os
 from pyrogram import Client, filters
 
 Bot = Client(
-"Simple pyrogram bot"
+"Simple pyrogram bot",
 bot_token = os.environ.get("BOT_TOKEN"),
 api_hash = int(os.enivron.get("API_HASH"),
 api_key = os.environ.get("API_KEY")
         )
 
-@Bot.on_message(filters.private & filters.command(["start"]))
+@Client.on_message(filters.private & filters.command(["start"]))
 async def start(bot, update):
     await update.reply_text(
     text=f"HI {update.from_user.mention}"
