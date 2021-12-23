164dcbf6a949dadb51057141d53dab022e7b88f1
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-12-23 18:20:08 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,11 +2,11 @@ import os
 from pyrogram import Client, filters
 
 Bot = Client(
-"Simple pyrogram bot"
-bot_token = os.environ.get("BOT_TOKEN")
-api_hash = int(os.enivron.get("API_HASH"),
-api_key = os.environ.get("API_KEY")
-        )
+    "Simple pyrogram bot"
+    bot_token = os.environ.get("BOT_TOKEN")
+    api_hash = int(os.enivron.get("API_HASH"),
+    api_key = os.environ.get("API_KEY")
+)
 
 @Bot.on_message(filters.private & filters.command(["start"]))
 async def start(bot, update):
