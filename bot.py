6e496898954297a1124860d93f06242879bf7fcd
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-12-23 18:52:26 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,17 @@
+import os 
+from pyrogram import Client, filters
+
+Bot = Client(
+"Simple pyrogram bot"
+bot_token = os.environ.get("BOT_TOKEN")
+api_hash = int(os.enivron.get("API_HASH"),
+api_key = os.environ.get("API_KEY")
+        )
+
+@Bot.on_message(filters.private & filters.command(["start"]))
+async def start(bot, update):
+    await update.reply_text(
+    text=f"HI {update.from_user.mention}"
+    )
+    
+    Bot.run()
