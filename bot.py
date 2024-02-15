2fc96df2e666f8a2b14273de5a3626c061d583fa
Zac <91833744+saminsumesh@users.noreply.github.com>
2024-02-16 03:11:08 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 Bot = Client(
 "Simple pyrogram bot",
@@ -10,28 +11,51 @@ api_id = int(os.environ.get("API_ID"))
 
 
 ABOUT_TXT = """ 
-× **Name** : [Gouri Hello Bot](https://t.me/gourihellobot)
-× **Creator** : [PaulWalker TG](https://t.mr/paulwalker_tg)
-× **FrameWork** : [PyroGram](https://pyrogram.org)
-× **Language** : [Python](https://python.org)
+× **Name** : [തെൻകുടം VIP Bot](https://t.me/gourihellobot)
+
+"""
+START_TXT = """
+Hi {} , Welcome to Thenkudam VIP BOT
 """
 
+HELP_TXT = """
+• YOU CAN PURCHASE VIP ACCESS OF OUR THENKUDAM VIP CHANNEL 🍑.
+• തെൻകുടം ചാനലിൻ്റെ VIP നിങ്ങൾക്ക് ഈ ബോട്ട് വഴി എടുക്കാം 🍑.
+
+"""
 @Bot.on_message(filters.private & filters.command(["start"]))
 async def start(bot, update):
-    await update.reply_text(
-    text=f"HI {update.from_user.mention} , Iam just a simple pyrogram bot , iam the first own project of my developer , really happy to have you here "
+    await update.reply_photo(
+        photo="AgACAgUAAxkBAAIrs2XOfBVlVcWYmUrJgTPPcDBCyvDbAAKytzEb9kZoVpVajrBFYePXAQADAgADeQADNAQ",
+        caption=START_TXT.format(update.from_user.mention),
+        reply_markup=InlineKeyboardMarkup(
+                InlineKeyboardButton("ഫ്രീ ഡെമോ 🍑", callback_data="demo"),
+                InlineKeyboardButton("വീഡിയോസ് ഗ്രൂപ്പ് 🔞", callback_data="group")
+                ][
+                InlineKeyboardButton("Help ❓", callback_data="help"),
+                InlineKeyboardButton("Admin 👮‍♂️", callback_data="admin"),
+        )
     )
+@Bot.on_callback_query()
+async def cb_buttons(bot, CallbackQuery):
+        if query.data == "help":
+                await bot.reply_message(HELP_TEXT),
+                reply_markup = InlinekeyboadMarkup(InlineKeyboardButton("Back 🔙", callback_data="start")),
+        elif query.data == "start":
+                await bot.reply_photo(
+                        photo="AgACAgUAAxkBAAIrs2XOfBVlVcWYmUrJgTPPcDBCyvDbAAKytzEb9kZoVpVajrBFYePXAQADAgADeQADNAQ",
+                        caption=START_TXT.format(bot.from_user.mention),
+                        reply_markup=InlineKeyboardMarkup(
+                InlineKeyboardButton("ഫ്രീ ഡെമോ 🍑", callback_data="demo"),
+                InlineKeyboardButton("വീഡിയോസ് ഗ്രൂപ്പ് 🔞", callback_data="group")
+                ][
+                InlineKeyboardButton("Help ❓", callback_data="help"),
+                InlineKeyboardButton("Admin 👮‍♂️", callback_data="admin"),
+                )
+        elif query.data == "demo":
+                await bot.reply_photo(
+                        photo="AgACAgUAAxkBAAIruGXOg16e38HqPSkmAAHQAAEoRWwx7_EAApG7MRsCnuhVKIeKGNzikusBAAMCAAN5AAM0BA"                        
+                )
+            
+
     
-@Bot.on_message(filters.private & filters.command(["help"]))
-async def help(bot, update):
-    await update.reply_sticker(
-    sticker="CAACAgUAAxkBAAEBi5ZhxIAlGIuYFRVXxXbMAkoe_hOzzQACJgUAAmF36FUlPPqbW2uijh4E"
-    )
-    
-@Bot.on_message(filters.private & filters.command(["about"]))
-async def about(bot, update):
-    await update.reply_text(
-    text=ABOUT_TXT,
-    disable_web_page_preview=True
-    )
-Bot.run()
