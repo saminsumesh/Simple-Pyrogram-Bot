0d01882a02b51515a097f5a87795f47e6f2c06b3
Zac <91833744+saminsumesh@users.noreply.github.com>
2024-02-16 05:21:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -40,7 +40,7 @@ async def start(bot, update):
     )
 @Bot.on_message(filters.private & filters.command(["file_id"]))
 async def media_id_handler(client, message: Message):
-    if message.from_user not message.reply_to_message:
+    if (message.from_user) not (message.reply_to_message):
             await bot.reply_text("Reply to something")
 else:
     media = getattr(message, message.media.value)
