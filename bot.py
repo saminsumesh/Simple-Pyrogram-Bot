fbdb25b7ea1d1a0ea1643a19e67aa416498dee37
Zac <91833744+saminsumesh@users.noreply.github.com>
2024-02-16 05:25:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,11 +38,9 @@ async def start(bot, update):
                 ]]
         )
     )
-@Bot.on_message(filters.private & filters.command(["file_id"]))
+
+@Bot.on_message(filters.forwarded & filters.media)
 async def media_id_handler(client, message: Message):
-  if not message.reply_to_message:
-            await bot.reply_text("Reply to something")
-  else:
     media = getattr(message, message.media.value)
     await message.reply_text(
         f"<code> {media.file_id} </code>", parse_mode=ParseMode.HTML, quote=True
